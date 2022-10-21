b0c7083cba1cdb40ddc0f024e1dd95485d537635
Krista Tian <kristatian@gmail.com>
2022-10-21 01:18:50 -0400

    added categories nav bar and filters

--- tmu-eats/src/home-flow/HomePage.js
+++ tmu-eats/src/home-flow/HomePage.js
@@ -1,26 +1,33 @@
 import '../App.css'
+//import { Header } from "./header/Header.js"
 
 const HomePage = () => {
     return (
-        <div className="App">
-            <header className="App-header">
-                <div className="fixed-header">
-                    <img src="../TMU-logo.png" alt ="TMU Logo" width="200" height="100"/>
-                    <h1>Eats</h1>
+        <div>
+            <div className="banner">
+                <img src="../TMU-logo.png" alt ="TMU Logo" width="250" height="100"/>
+                <h1>Eats</h1>
+            </div>
+            <div className="navbar">
+                <div class="dropdown">
+                    <button class="dropdown-btn">Filters</button>
+                    <div class="dropdown-content">
+                        <a href="#">Best Rated</a>
+                        <a href="#">Offers</a>
+                        <a href="#">Fastest Near Me</a>
+                    </div>
                 </div>
-                <div className="category-bar">
-                    
-                    <a href="#" class="">Italian</a>
-                    <a href="#" class="">Chinese</a>
-                    <a href="#" class="">Indian</a>
-                    <a href="#" class="">Korean</a>
-                    <a href="#" class="">Fast Food</a>
-                </div>
-            </header>
+                <div class="categories">
+                    <a href="#" class="nav1">Italian</a>
+                    <a href="#" class="nav2">Chinese</a>
+                    <a href="#" class="nav3">Indian</a>
+                    <a href="#" class="nav4">Korean</a>
+                    <a href="#" class="nav5">Fast Food</a>
+                </div> 
+            </div>
         </div>
     );
 
 };
-
 
 export default HomePage
