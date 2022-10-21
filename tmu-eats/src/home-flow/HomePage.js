a46c8bf0cd4145927928ca4fbfe9c07478aaaa68
Krista Tian <kristatian@gmail.com>
2022-10-20 22:00:44 -0400

    added categories for nav bar

--- tmu-eats/src/home-flow/HomePage.js
+++ tmu-eats/src/home-flow/HomePage.js
@@ -8,6 +8,14 @@ const HomePage = () => {
                     <img src="../../public/TMU-logo.png" alt ="TMU Logo" width="200" height="100"/>
                     <h1>Eats</h1>
                 </div>
+                <div className="category-bar">
+                    
+                    <a href="#" class="">Italian</a>
+                    <a href="#" class="">Chinese</a>
+                    <a href="#" class="">Indian</a>
+                    <a href="#" class="">Korean</a>
+                    <a href="#" class="">Fast Food</a>
+                </div>
             </header>
         </div>
     );
