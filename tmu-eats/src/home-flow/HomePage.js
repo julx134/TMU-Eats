30312f431da6db0041ee2da674358f4d10b67d0d
will1415 <114450715+will1415@users.noreply.github.com>
2022-10-20 22:40:24 -0400

    Updated Logo With Figma Version

--- tmu-eats/src/home-flow/HomePage.js
+++ tmu-eats/src/home-flow/HomePage.js
@@ -5,8 +5,8 @@ const HomePage = () => {
         <div className="App">
             <header className="App-header">
                 <div className="fixed-header">
-                    <img src="../TMU-logo.png" alt ="TMU Logo" width="200" height="100"/>
-                    <h1>Eats</h1>
+                    <img src="../TMU-logo.png" alt ="TMU Logo" width="250" height="100"/>
+                    <title>Eats</title>
                 </div>
             </header>
         </div>
