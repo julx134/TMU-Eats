108bac35fdd93449a106f6e229f1b2382400b993
julx134 <julesgammad56@gmail.com>
2022-10-25 12:58:13 -0400

    Added Firebase Login Functionalities
    -Finished login with google
    -Finished login using email and password
    -Configured firebase backend

--- tmu-eats/src/App.js
+++ tmu-eats/src/App.js
@@ -1,25 +1,9 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
 import LoginPage from "../src/login-flow/LoginPage";
 import Cart from "../src/item-flow/Cart";
 import HomePage from "../src/home-flow/HomePage";
 import "antd/dist/antd.min.css";
-import { initializeApp } from "firebase/app";
-import "firebase/auth";
-import "firebase/firestore"
-
-const firebaseConfig = {
-  aapiKey: "AIzaSyAYa6kBVdSja_AgTSzVzekC0Pul7u-7NvE",
-  authDomain: "tmu-eats.firebaseapp.com",
-  projectId: "tmu-eats",
-  storageBucket: "tmu-eats.appspot.com",
-  messagingSenderId: "875772432483",
-  appId: "1:875772432483:web:f8dea098c129eff6fbe856",
-  measurementId: "G-KSZT30CR1X",
-};
-
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
 
 function App() {
   return (
@@ -34,4 +18,3 @@ function App() {
 }
 
 export default App;
-export const app;
--- tmu-eats/src/login-flow/LoginPage.js
+++ tmu-eats/src/login-flow/LoginPage.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { Row, Col } from "antd";
 import Graphic from "./components/Graphic";
 import LoginModule from "./components/LoginModule";
