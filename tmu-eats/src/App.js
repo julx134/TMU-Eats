b3113cd2f406691a518024ecd7e4b9858c800b11
julx134 <julesgammad56@gmail.com>
2022-10-25 09:14:00 -0400

    added firebase config

--- tmu-eats/src/App.js
+++ tmu-eats/src/App.js
@@ -3,6 +3,23 @@ import LoginPage from "../src/login-flow/LoginPage";
 import Cart from "../src/item-flow/Cart";
 import HomePage from "../src/home-flow/HomePage";
 import "antd/dist/antd.min.css";
+import { initializeApp } from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore"
+
+const firebaseConfig = {
+  aapiKey: "AIzaSyAYa6kBVdSja_AgTSzVzekC0Pul7u-7NvE",
+  authDomain: "tmu-eats.firebaseapp.com",
+  projectId: "tmu-eats",
+  storageBucket: "tmu-eats.appspot.com",
+  messagingSenderId: "875772432483",
+  appId: "1:875772432483:web:f8dea098c129eff6fbe856",
+  measurementId: "G-KSZT30CR1X",
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
 
 function App() {
   return (
@@ -17,3 +34,4 @@ function App() {
 }
 
 export default App;
+export const app;
